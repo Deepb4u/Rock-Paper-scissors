6518c284dd6d26c9b9578d2a59c00b9a2ec94e9c
Deepb4u <deepbansode1@gmail.com>
2024-07-07 00:28:14 +0530

    initial commit

---
+++ Stone_paper_scissors.js
@@ -0,0 +1,53 @@
+let usecount= document.querySelector(".usecount");
+let userScore = 0;
+let compScore = 0;
+let compcount = document.querySelector(".cmpcount");
+let Mesg = document.querySelector("#Mesg");
+
+
+let choices = document.querySelectorAll(".choice");
+
+const GenCompChoice = () =>{
+    const Options = ["rock","paper","scissor"];
+    const ranIdx =  Math.floor(Math.random () * 3);
+    return Options[ranIdx];
+}
+
+const showWinner = (winner,UserChoice,compchoice) => {
+    if (winner){
+        userScore++;
+        usecount.innerHTML=userScore;
+        // Mesg.innerText = `You Win! ${UserChoice} beat ${comchoice}`;
+        Mesg.innerText = "You Win";
+    }else{
+        compScore++;
+        compcount.innerHTML= compScore;
+        Mesg.innerHTML=`You Lose! ${comchoice} beat ${UserChoice}`;
+    }
+}
+
+const playGame = (UserChoice) =>{
+    // Generate comuter choi
+    const compchoice = GenCompChoice();
+
+    if(UserChoice === compchoice){
+        Mesg.innerHTML = "Drow";
+    }else{
+        let Userwin = true;
+        if(UserChoice =="rock"){
+            Userwin = compchoice ==="paper" ? false : true;
+        }else if(UserChoice==="paper"){
+            Userwin = compchoice === "scissor" ? false : true;
+        }else{
+            Userwin = compchoice === "rock" ? false : true;
+        }
+        showWinner(Userwin ,UserChoice ,compchoice);
+    }
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click",() =>{
+        const UserChoice = choice.getAttribute("id");
+        playGame(UserChoice);
+    })
+})
